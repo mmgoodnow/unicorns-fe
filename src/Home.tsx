05355c93a042e111d7da87120d66d4b36cdbeebe
james-herbstritt <james.herbstritt@gmail.com>
2020-07-03 17:16:14 -0400

    Add basic styling for landing page

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,9 +4,20 @@ import { Link } from "react-router-dom";
 const Home = () => {
 	return (
 		<div>
-			<Link to="/login">Log In</Link>
-			<br></br>
-			<Link to="/signup">Sign Up</Link>
+			<div className="text-center">
+				<h2>Welcome to Unicorns!</h2>
+			</div>
+			<div className="container">
+				<div className="col-6 justify-content-center mx-auto">
+					<Link className="btn btn-block btn-primary" to="/login">
+						Log In
+					</Link>
+					<br></br>
+					<Link className="btn btn-block btn-success" to="/signup">
+						Sign Up
+					</Link>
+				</div>
+			</div>
 		</div>
 	);
 };
