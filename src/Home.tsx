b1e312f066b88a2479828afcc9d52c0fb76b24ff
james-herbstritt <james.herbstritt@gmail.com>
2020-07-06 20:09:12 -0400

    Add welcome message

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,10 @@ const Home = () => {
 				<h2>Welcome to Unicorns!</h2>
 			</div>
 			<div className="container">
-				<div className="col-6 justify-content-center mx-auto">
+				<div
+					className="col-6 justify-content-center mx-auto
+                        align-middle"
+				>
 					<Link className="btn btn-block btn-primary" to="/login">
 						Log In
 					</Link>
