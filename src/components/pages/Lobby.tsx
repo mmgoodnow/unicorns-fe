af2263d92c68fa573b3771267bed617dd8289419
Michael Goodnow <mmgoodnow@gmail.com>
2020-08-25 02:43:04 -0400

    Add back 'logged in as'

--- src/components/pages/Lobby.tsx
+++ src/components/pages/Lobby.tsx
@@ -1,15 +1,17 @@
 import React, { useCallback } from "react";
 import { logout } from "../../store/system/actions";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { userSelector } from "../../store/system/selectors";
 
 export function Lobby() {
 	const dispatch = useDispatch();
+	const user = useSelector(userSelector);
 	const handleLogout = useCallback(() => {
 		dispatch(logout());
 	}, [dispatch]);
 	return (
 		<div>
-			You're in the lobby.
+			You're logged in as {JSON.stringify(user)}
 			<button className="btn btn-primary" onClick={handleLogout}>
 				Logout
 			</button>
